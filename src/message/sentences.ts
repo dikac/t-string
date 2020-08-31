b00e18082dcebf2988217ac4d23d0da459111c83
dikac <rigel234@yahoo.com>
2020-09-01 03:34:13 +0700

    add truncate, sentence, sentences

---
+++ src/message/sentences.ts
@@ -0,0 +1,41 @@
+import ValueOf from "@dikac/t-value/value-of/value-of";
+import NotEmpty from "../boolean/not-empty";
+import Value from "@dikac/t-value/value";
+import Validatable from "@dikac/t-validatable/validatable";
+
+export default class Sentences implements ValueOf<string>, Value<string[]>, Validatable {
+
+    constructor(
+        public valid : boolean,
+        public value : string[],
+        public expectation : {invalid : string[], valid : string[]},
+        public type : string[]
+    ) {
+    }
+
+    valueOf(): string {
+
+        return this.message;
+    }
+
+    toString() : string {
+
+        return this.message;
+    }
+
+    get message() : string {
+
+        let messages : string[] = [];
+
+        messages.push(...this.value);
+
+        messages.push(...(this.valid ? this.expectation.valid : this.expectation.invalid));
+
+        messages.push(...this.type);
+
+        messages = messages.filter(NotEmpty);
+
+        return messages.join(' ');
+    }
+
+}
---
+++ src/truncate.ts
@@ -0,0 +1,13 @@
+import * as Escape from "escape-string-regexp";
+import {truncate} from "voca";
+
+/**
+ * @param string
+ * @param length
+ * @param end
+ * default "..."
+ */
+const Truncate : (string : string, length : number, end?: string) => string = truncate;
+
+export default Truncate;
+
